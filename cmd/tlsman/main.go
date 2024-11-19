ef68301897a63ea2b06e71d40a73e0aef93a16ff
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-11-19 13:06:46 +0100

    Add go-kit + run go mod tidy
    Base application setup from go-kit
    
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- cmd/tlsman/main.go
+++ cmd/tlsman/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/jantytgat/go-kit/pkg/application"
+	"github.com/jantytgat/go-kit/pkg/semver"
+	"github.com/jantytgat/go-kit/pkg/slogd"
+	"github.com/spf13/cobra"
+)
+
+var (
+	buildVersion string
+
+	appName   = "tlsman"
+	appTitle  = "TLSMAN"
+	appBanner = ""
+)
+
+var (
+	version semver.Version
+	ctx     context.Context
+
+	tlsmanCmd = &cobra.Command{
+		Use:  appName,
+		RunE: tlsmanFuncE,
+	}
+)
+
+func tlsmanFuncE(cmd *cobra.Command, args []string) error {
+	return nil
+}
 
 func main() {
-	fmt.Println("TLSMAN")
+	var err error
+
+	slogd.Init(slogd.LevelInfo, false)
+	slogd.RegisterColoredTextHandler(os.Stderr, true)
+
+	if version, err = semver.Parse(buildVersion); err != nil {
+		panic(err)
+	}
+
+	ctx = slogd.WithContext(context.Background())
+
+	application.New(appName, appTitle, appBanner, version)
+	application.RegisterCommand(tlsmanCmd)
+
+	if err = application.Run(ctx); err != nil {
+		slogd.Logger().LogAttrs(ctx, slogd.LevelError, "error running application", slog.Any("error", err))
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
